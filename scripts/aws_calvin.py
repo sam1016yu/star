8ba3e150d9c9fa91de349942d3d809a21eccd1dc
sam1016yu <sam1016yu@gmail.com>
2021-02-27 13:11:14 -0500

    chang ip

--- scripts/aws_calvin.py
+++ scripts/aws_calvin.py
@@ -9,10 +9,10 @@ ins = [line.split("|")[0] for line in ips]
 outs = [line.split("|")[1] for line in ips]
 
 hostname = subprocess.run(["hostname"],capture_output=True).stdout.decode('utf8')
-# hostname = "node0"
+# hostname = "node2"
 id = re.findall("(?<=node)[0-9]",hostname)[0]
 
-port = 9000
+port = "9000"
 ratio = int(sys.argv[1])
 
 # ratios = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
@@ -40,8 +40,10 @@ def get_cmd(n, i):
 # for lock in locks:
 #   for ratio in ratios:    
     # for i in range(3):
-cmd = get_cmd(n, 0)
-cmd = '../bench_tpcc --logtostderr=1 --id=%s --servers="%s" --protocol=Calvin --partition_num=%d --threads=20 --batch_size=10000 --replica_group=8 --lock_manager=%d --query=mixed --neworder_dist=%d --payment_dist=%d' % (id, cmd, 20*n, lock, ratio, ratio)
+ips = [ip+":"+port for ip in ins]
+# print(ips)
+ips = ";".join(ips)
+cmd = "../bench_tpcc --logtostderr=1 --id=%s --servers='%s' --protocol=Calvin --partition_num=%d --threads=20 --batch_size=10000 --replica_group=8 --lock_manager=%d --query=mixed --neworder_dist=%d --payment_dist=%d" % (id, ips, 20*n, lock, ratio, ratio)
 print("###{}".format(ratio))
 # print(cmd.split(' '))
 subprocess.run(cmd.split(' '))
