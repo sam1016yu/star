412ecac6944c305d1b97ff57cf2fb8c3a9d488c5
sam1016yu <sam1016yu@gmail.com>
2021-02-27 00:45:52 -0500

    modified aws_calvin script for easier running

--- scripts/aws_calvin.py
+++ scripts/aws_calvin.py
@@ -1,16 +1,23 @@
 import sys
+import subprocess
+import re
 
 ips = [line.strip() for line in open("ips.txt", "r")]
 n = len(ips)
 
-ins = [line.split("\t")[0] for line in ips]
-outs = [line.split("\t")[1] for line in ips]
+ins = [line.split("|")[0] for line in ips]
+outs = [line.split("|")[1] for line in ips]
 
-id = int(sys.argv[1]) 
-port = int(sys.argv[2]) 
+hostname = subprocess.run(["hostname"],capture_output=True).stdout.decode('utf8')
+# hostname = "node0"
+id = re.findall("(?<=node)[0-9]",hostname)[0]
 
-ratios = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-locks = [2, 4, 6]
+port = 9000
+ratio = int(sys.argv[1])
+
+# ratios = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+# locks = [1]
+lock = 1
 
 def get_cmd(n, i):
   cmd = ""
@@ -23,15 +30,18 @@ def get_cmd(n, i):
       cmd += outs[j] + ":" + str(port+i)
   return cmd
 
-for lock in locks:
-  for ratio in ratios:    
-    for i in range(3):
-      cmd = get_cmd(n, i)
-      print('./bench_ycsb --logtostderr=1 --id=%d --servers="%s" --protocol=Calvin --partition_num=%d --threads=12 --batch_size=10000 --replica_group=4 --lock_manager=%d --read_write_ratio=90 --cross_ratio=%d' % (id, cmd, 12*n, lock, ratio))
+# for lock in locks:
+#   for ratio in ratios:    
+#     for i in range(3):
+#       cmd = get_cmd(n, i)
+#       print('./bench_ycsb --logtostderr=1 --id=%d --servers="%s" --protocol=Calvin --partition_num=%d --threads=12 --batch_size=10000 --replica_group=4 --lock_manager=%d --read_write_ratio=90 --cross_ratio=%d' % (id, cmd, 12*n, lock, ratio))
 
 
-for lock in locks:
-  for ratio in ratios:    
-    for i in range(3):
-      cmd = get_cmd(n, i)
-      print('./bench_tpcc --logtostderr=1 --id=%d --servers="%s" --protocol=Calvin --partition_num=%d --threads=12 --batch_size=10000 --replica_group=4 --lock_manager=%d --query=mixed --neworder_dist=%d --payment_dist=%d' % (id, cmd, 12*n, lock, ratio, ratio))
+# for lock in locks:
+#   for ratio in ratios:    
+    # for i in range(3):
+cmd = get_cmd(n, 0)
+cmd = '../bench_tpcc --logtostderr=1 --id=%s --servers="%s" --protocol=Calvin --partition_num=%d --threads=20 --batch_size=10000 --replica_group=4 --lock_manager=%d --query=mixed --neworder_dist=%d --payment_dist=%d' % (id, cmd, 12*n, lock, ratio, ratio)
+print("###{}".format(ratio))
+# print(cmd.split(' '))
+subprocess.run(cmd.split(' '))
